f37df06e12db1eeeec700010a6f46e8cd57fd1d5
James <jamesthe1sky@gmail.com>
2022-02-06 12:06:29 -0600

    Add files via upload

---
+++ BarrelRoll.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace Jamesthe1.BarrelRoll {
+    [BepInPlugin (pluginGuid, pluginName, pluginVersion)]
+    [BepInProcess ("dyingsun.exe")]
+    public class BarrelRoll : BaseUnityPlugin {
+        // Variables so other classes and this one may use them
+        public const string pluginGuid = "jamesthe1.hotds.barrelroll";
+        public const string pluginName = "BarrelRoll";
+        public const string pluginVersion = "1.0.0.0";
+
+        void Awake () {
+            InitHarmony ();
+            Logger.LogInfo (pluginName + " has fully loaded and is ready to go!");
+        }
+
+        private void InitHarmony () {
+            Harmony harmony = new Harmony (pluginGuid);
+            Logger.LogInfo ("Patching with harmony...");
+            harmony.PatchAll ();
+        }
+    }
+}
---
+++ Unit_FixedUpdate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Jamesthe1.BarrelRoll {
+    [HarmonyPatch (typeof (Unit), "FixedUpdate")]
+    public class Unit_FixedUpdate {
+        // Postfix comes after the function, a la post
+        public static void Postfix (Unit __instance) {
+            if (!__instance.isPlayer && !Game.isSimulationPaused)
+                __instance.rotation *= Quaternion.Euler (0, 0, 5);
+        }
+    }
+}
